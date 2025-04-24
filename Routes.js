a4ac1d8d5b614afc3b7fd3e7c02a8527823722a0
hichamaki <hichamakil2018@gmail.com>
2025-04-24 20:37:29 +0100

    new things

---
+++ Routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path=require('path')
+const dataFilePath = path.join(__dirname, 'data.json');
+
+const readData = () => {
+  const rawData = fs.readFileSync(dataFilePath);
+  return JSON.parse(rawData);
+};
+const writeData = (data) => {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+  };
+app.use(express.json());
+
+// app.listen(port,()=>{
+//     console.log('server is runnig')
+// })
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+//   });
+// app.post('/user/:name/:age', (req, res) => {
+//     const {name,age}=req.params;
+//     res.send(`User ${name} is ${age} years old.`);
+// });
+app.post('/user', (req, res) => {
+    const { name, age } = req.body;
+    const data = readData();
+  
+    const newUser = {
+      id: data.users.length + 1, 
+      name,
+      age
+    };
+  
+    data.users.push(newUser);
+    writeData(data);
+  
+    res.status(201).json(newUser);
+});
